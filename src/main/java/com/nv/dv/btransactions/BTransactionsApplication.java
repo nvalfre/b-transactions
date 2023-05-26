5819eab45f7d14cb8603c511f5c6a0734becb9f8
nvalfre <kokevalfre@gmail.com>
2023-05-26 09:13:10 -0300

    1st commit

---
+++ src/main/java/com/nv/dv/btransactions/BTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.nv.dv.btransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BTransactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nv/dv/btransactions/BTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nv.dv.btransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
